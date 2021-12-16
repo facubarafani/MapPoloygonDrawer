f47ab0480781ae8a4ce29e2c7f03c5d55a71b2f2
Facundo Barafani <facu1910@gmail.com>
2021-12-16 15:42:49 -0300

    Add logic for adding points and drawing polygon

--- MapPolygonDrawer/Controller/ViewController.swift
+++ MapPolygonDrawer/Controller/ViewController.swift
@@ -14,6 +14,8 @@ import PromiseKit
 class ViewController: UIViewController {
     @IBOutlet weak var addFloatingButton: MDCFloatingButton!
     @IBOutlet weak var mapView: GMSMapView!
+    var isCreatingPolygon: Bool = false
+    var currentMarkers = [GMSMarker]()
     
     var userLocationCoordinate: CLLocationCoordinate2D?
     override func viewDidLoad() {
@@ -31,7 +33,7 @@ class ViewController: UIViewController {
     @objc func setText() {}
     
     @objc func setUI() {
-        addFloatingButton.setImage(R.image.images.add_black_24dp(), for: .normal)
+        addFloatingButton.setImage(R.image.images.add(), for: .normal)
         addFloatingButton.setImageTintColor(.white, for: .normal)
         addFloatingButton.setBackgroundColor(R.color.colors.primary(), for: .normal)
     }
@@ -62,14 +64,81 @@ class ViewController: UIViewController {
         mapView.delegate = nil
     }
     
+    @IBAction func addFloatingButtonPressed(_ sender: Any) {
+        // if last state before pressing button is user not creating polygon -> now it turns into true
+        if !isCreatingPolygon {
+            // sets cross icon when isCreatingPolygon is turning into true
+            addFloatingButton.setImage(R.image.images.close(), for: .normal)
+        } else {
+            // sets add icon when isCreatingPolygon is turning into false
+            addFloatingButton.setImage(R.image.images.add(), for: .normal)
+        }
+        // Changes variable value to inverse
+        isCreatingPolygon = !isCreatingPolygon
+    }
     
 }
 
 extension ViewController: GMSMapViewDelegate {
     func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        // if user is not creating polygon -> then ignore taps at screen
+        if !isCreatingPolygon {
+            return
+        }
         let marker = GMSMarker()
         marker.position = coordinate
         marker.map = mapView
+        currentMarkers.append(marker)
+        mapView.clear()
+        let rect = reorderMarkersClockwise(mapView)
+        for mark in currentMarkers {
+            mark.map = mapView
+        }
+        let polygon = GMSPolygon(path: rect)
+        polygon.fillColor = UIColor(red: 0.25, green: 0, blue: 0, alpha: 0.05);
+        polygon.strokeColor = .black
+        polygon.strokeWidth = 2
+        polygon.map = mapView
+    }
+    
+    func reorderMarkersClockwise(_ mapView: GMSMapView) -> GMSMutablePath {
+        let rect = GMSMutablePath()
+        let arr = currentMarkers.map{$0.position}.sorted(by: isLess)
+        for pos in arr {
+            rect.add(pos)
+        }
+        
+        return rect
+    }
+    
+    func isLess(_ a: CLLocationCoordinate2D, _ b: CLLocationCoordinate2D) -> Bool {
+        let center = getCenterPointOfPoints()
+        
+        if (a.latitude >= 0 && b.latitude < 0) {
+            return true
+        } else if (a.latitude == 0 && b.latitude == 0) {
+            return a.longitude > b.longitude
+        }
+        
+        let det = (a.latitude - center.latitude) * (b.longitude - center.longitude) - (b.latitude - center.latitude) * (a.longitude - center.longitude)
+        if (det < 0) {
+            return true
+        } else if (det > 0) {
+            return false
+        }
+        
+        let d1 = (a.latitude - center.latitude) * (a.latitude - center.latitude) + (a.longitude - center.longitude) * (a.longitude - center.longitude)
+        let d2 = (b.latitude - center.latitude) * (b.latitude - center.latitude) + (b.longitude - center.longitude) * (b.longitude - center.longitude)
+        return d1 > d2
+    }
+    
+    func getCenterPointOfPoints() -> CLLocationCoordinate2D {
+        let arr = currentMarkers.map {$0.position}
+        let s1: Double = arr.map{$0.latitude}.reduce(0, +)
+        let s2: Double = arr.map{$0.longitude}.reduce(0, +)
+        let c_lat = arr.count > 0 ? s1 / Double(arr.count) : 0.0
+        let c_lng = arr.count > 0 ? s2 / Double(arr.count) : 0.0
+        return CLLocationCoordinate2D.init(latitude: c_lat, longitude: c_lng)
     }
 }
 
